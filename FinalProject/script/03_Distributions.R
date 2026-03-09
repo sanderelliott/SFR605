7ef5406c1c0f5ffe24251621b9dc95c9ba0c26c9
sanderelliott <sander.elliott@maine.edu>
2026-03-09 13:58:18 -0400

    first ks test

---
+++ FinalProject/script/03_Distributions.R
@@ -0,0 +1,16 @@
+## Evaluate Data
+
+## KS tests
+
+yrs <- unique(penob_events$y_leave)
+yrs
+
+
+for (i in 1:length(yrs)) {
+  yr <- yrs[i]
+  yr.rows <- penob_events[penob_events[,"y_leave"] == yr, 'd_leave']
+    assign(paste0("penob.d.", yr), yr.rows$d_leave)
+}
+
+ks.test(penob.d.2016, penob.d.2018, alternative = "two.sided")
+x
