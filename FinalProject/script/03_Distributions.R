04260052296dbd6f8b758542b34871c2a4ad00a8
sanderelliott <sander.elliott@gmail.com>
2026-03-12 12:46:53 -0400

    binning departures

--- FinalProject/script/03_Distributions.R
+++ FinalProject/script/03_Distributions.R
@@ -1,4 +1,4 @@
-## Evaluate Data
+1## Evaluate Data
 
 ## KS tests
 
@@ -12,5 +12,59 @@ for (i in 1:length(yrs)) {
     assign(paste0("penob.d.", yr), yr.rows$d_leave)
 }
 
-ks.test(penob.d.2016, penob.d.2018, alternative = "two.sided")
+ggplot(depart_m, aes(x = d_leave)) +
+  geom_histogram(bins = 52, boundary = 0.5) +
+
+  theme_classic() +
+ 
+  labs(
+    title = "Departures by Week Over Multiple Years",
+    subtitle = "Shortnose Sturgeon from Penobscot River",
+    x = "Week",
+    y = "Frequency")
+
+ggplot(depart_m, aes(x = d_leave, y = y_leave)) +
+  geom_point() +
+  geom_vline(xintercept =240) +
+  geom_vline(xintercept =136) +
+  theme_classic() +
+  xlab("Departure Date") + 
+  ylab("Year")
+
+
+ggplot(depart_m, aes(x = d_leave)) +
+  geom_histogram(bins = 52, boundary = 0.5) +
+  geom_vline(xintercept =240) +
+  geom_vline(xintercept =136) +
+  coord_cartesian(xlim = c(1, 365)) +
+  theme_classic() +
+  facet_wrap(~y_leave) +
+  labs(
+    title = "Departures by Week Over Multiple Years",
+    subtitle = "Shortnose Sturgeon from Penobscot River",
+    x = "Week",
+    y = "Frequency")
+
+ggplot(depart_m, aes(x = d_leave)) +
+  geom_histogram(aes(y = ..density..), bins = 52, fill = "grey80", color = "white") +
+  geom_density(color = "blue", size = 1) +
+  coord_cartesian(xlim = c(1, 365)) +
+  geom_vline(xintercept =240) +
+  geom_vline(xintercept =136) +
+  theme_classic() +
+  labs(
+    title = "Departure Timing Distribution",
+    subtitle = "Shortnose Sturgeon from Penobscot River",
+    x = "Julian Day",
+    y = "Density"
+  )
+
+depart_early <- depart_m %>% 
+  filter(d_leave < 240)
+
+ggplot(depart_early, aes(x = d_leave, y = y_leave)) +
+  geom_point() +
+  theme_classic() +
+  xlab("Departure Date") + 
+  ylab("Year")
 
